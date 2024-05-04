0c77941d21988ef15d1372b7980aada7d9878063
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2024-05-04 19:41:35 +0900

    Vue3 Pixiのサンプルを実装

---
+++ src/components/demo.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import type { GraphicsInst } from 'vue3-pixi'
+
+function drawArc(e: GraphicsInst) {
+  e.beginFill('#4f455c')
+  e.arc(0, 0, 100, 0, Math.PI, false)
+}
+</script>
+
+<template>
+  <Graphics :x="120" :y="120" @render="drawArc" />
+</template>
